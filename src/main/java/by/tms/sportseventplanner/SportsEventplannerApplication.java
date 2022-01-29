11bff98d5618fe9063e8c2a9caa854b9f989db22
KLISHEVSKIY\Kliis <i.klishevskiy@mail.ru>
2022-01-29 22:10:07 +0300

    Initial commit

---
+++ src/main/java/by/tms/sportseventplanner/SportsEventplannerApplication.java
@@ -0,0 +1,13 @@
+package by.tms.sportseventplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsEventplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsEventplannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/tms/sportseventplanner/SportsEventplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tms.sportseventplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsEventplannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
