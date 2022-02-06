e92a2811930ef84ba3ea2fa339b20b120e87cec6
KLISHEVSKIY\Kliis <i.klishevskiy@mail.ru>
2022-02-06 23:23:43 +0300

    add weather init data.

---
+++ src/main/resources/db/migration/V6__Init_weather.sql
@@ -0,0 +1,8 @@
+insert into weather (id, city, date, description, humidity, pressure, temp, visibility, wind_speed)
+values (null, 'Minsk', '2022-02-03', 'light snow', 88, 1000, -1.0, 71, 7.219);
+
+insert into weather (id, city, date, description, humidity, pressure, temp, visibility, wind_speed)
+values (null, 'Vitebsk', '2022-02-04', 'light snow', 99, 997, -5.3, 88, 5.111);
+
+insert into weather (id, city, date, description, humidity, pressure, temp, visibility, wind_speed)
+values (null, 'Gomel', '2022-02-05', 'light snow', 95, 991, -2.5, 91, 3.331);
